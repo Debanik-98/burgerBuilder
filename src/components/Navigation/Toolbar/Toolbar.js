448748694d1c0dc1339a14951f07cc336383e8b4
Debanik Mishra <Debanik.Mishra@walmartlabs.com>
2020-08-16 19:57:49 +0530

    Changed Folder Structure, fixed bugs for mobile UI

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,16 @@
-import React from "react"
-import classes from "./Toolbar.module.css"
-import Logo from "../../Logo/Logo"
-import NavigationItems from "../NavigationItems/NavigationItems"
+import React from 'react';
+
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo height="80%" />
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,25 @@
-import React, { Component } from "react";
-import classes from "./Modal.module.css"
-import Aux from "../../../hoc/Aux"
-import Backdrop from "../Backdrop/Backdrop"
+import React, { Component } from 'react';
+
+import classes from './Modal.module.css';
+import Aux from '../../../hoc/Aux/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
-        if (nextProps.show !== this.props.show) {
-            return true;
-        }
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
     }
 
     render() {
         return (
-            // eslint-disable-next-line no-unused-expressions
             <Aux>
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div className={classes.Modal}
+                <div
+                    className={classes.Modal}
                     style={{
                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
                         opacity: this.props.show ? '1' : '0'
@@ -26,7 +29,6 @@ class Modal extends Component {
             </Aux>
         )
     }
-
-
 }
+
 export default Modal;
